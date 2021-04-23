1e2243d5598d4eb8e2abc27de09cf17d8ec1f69e
ienglehart <68967971+ienglehart@users.noreply.github.com>
2021-04-22 23:04:46 -0400

    General updates
    API data now printed on click, still needs formatting. Layout updated, along with other general css changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var locationInputEl = document.querySelector("#location");
 var weatherContainerEl = document.querySelector('#print-here');
 
+// function that begins API call when a location is submitted
 var formSubmitHandler = function(event){
   console.log("click");
   event.preventDefault();
@@ -10,16 +11,15 @@ var formSubmitHandler = function(event){
   //console.log(location);
   if (location){
     getWeather(location);
-
+    //displayWeather(); //testing printing in elements
     locationInputEl.value = "";
   }else{
     alert("theres nothing here!");
   }
 }
 
+// Function to handle API call and begin the function to print recieved data
 var getWeather = function(location) {
-  // format the github api url
-  // var location = locationInputEl;
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ location +'&appid=339dea2bbca309f35c984f29f1697e78';
   
   // make a get request to url
@@ -30,7 +30,7 @@ var getWeather = function(location) {
         console.log(response);
         response.json().then(function(data) {
           console.log(data);
-          displayWeather(data, location);
+          displayWeather(data);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -41,13 +41,20 @@ var getWeather = function(location) {
     });
 };
 
+// Function to print weather data from api to existing html elements
+var displayWeather = function(data) {
 
-var displayWeather = function(data, location) {
-  var weatherEl = document.createElement("a");
-  var textnode = document.createTextNode = "data main";
-  weatherEl.appendChild(textnode);
+  var nameEl = document.getElementById("location-name");
+  var nameData = data.name; // data passed in from api here
+  nameEl.append = nameData;
 
-  document.getElementById("weather").appendChild(weatherEl);
+  var tempEl = document.getElementById("location-temp");
+  var tempData = "hi, this will be location"; // data passed in from api here
+  tempEl.innerText = tempData;
+
+  var weatherEl = document.getElementById("location-weather");
+  var weatherData = "hi, this will be location"; // data passed in from api here
+  weatherEl.innerText = weatherData;
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
